09139ec721e7b56cc73c40babbed25317e57334d
Viviane Santos <viviane.ssantosp@gmail.com>
2020-09-18 19:11:33 -0300

    Feat(exercicio) exercicios de JavaScript

---
+++ exercicios-para-casa/viviane-santos/exercicios/uriJudge/02-exercicio/scrips.js
@@ -0,0 +1,1 @@
+//Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas. 
