91c09592db8ef4299e2ed55df20d344ea3df3f30
Thaysa <thaysacgms@gmail.com>
2020-09-19 10:22:57 -0300

    exercícios js

---
+++ exercicios-para-casa/thaysa/exercicios/funcoes/02-exercicio/script.js
@@ -0,0 +1,8 @@
+function multiplicacao(numero1, numero2) {
+    numero1 = prompt("Digite um número");
+    numero2 = prompt("Digite outro número")
+    let resultado = numero1 * number2;
+    alert(resultado);
+}
+
+multiplicacao();
