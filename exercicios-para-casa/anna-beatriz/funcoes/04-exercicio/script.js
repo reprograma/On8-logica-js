cdec85edf1b9e869c69e4563f70975fc81767585
Anna Beatriz <abacardoso@hotmail.com>
2020-09-18 23:58:26 -0300

    add exercicio casa js anna-beatriz

---
+++ exercicios-para-casa/anna-beatriz/funcoes/04-exercicio/script.js
@@ -0,0 +1,17 @@
+/*
+4. Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+*/
+
+
+function anoNascimento (num) { 
+    return num; 
+  }
+
+  if (anoNascimento >= 18) {
+    console.log ("Você é maior de idade")
+  }
+
+  else if (anoNascimento < 18) {
+    console.log ("Você é menor de idade")
+  }
+
---
+++ exercicios-para-casa/anna-beatriz/funcoes/05-exercicio/script.js
@@ -0,0 +1,13 @@
+/*
+5. Crie uma função que receba um nome e retorne uma saudação na forma de uma string.
+ `Exemplo: "Olá, Natália"`
+ */
+
+ const nome = (prompt("Informe seu nome"))
+
+ function saudacao (nome1) {
+     nome = nome1
+ }
+
+ alert("Olá, " + saudacao (nome1) )
+
